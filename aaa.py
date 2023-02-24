cb9f7594a8b6ed57b00330b6677afacbe616854f
JangRuBin <wkdfnqls2465@gmail.com>
2023-02-24 14:54:58 +0900

    js와 py맛보기

---
+++ aaa.py
@@ -0,0 +1,16 @@
+rubin = 1
+print(rubin)
+rubin = "바보"
+print(rubin)
+
+kdt = {
+  "305호" : "홀쭉",
+  "공욱재" : "미남",
+}
+print(kdt["공욱재"])
+
+kdt_order = ["우리는", "개발자", "입니다"]
+print(kdt_order[0],kdt_order[1],kdt_order[2])
+
+for index in kdt_order:
+  print(index)
